a7b375c8b34c223034f8c70805125acb68c20165
Tanja Kunz <tk.kunz@gmx.at>
2019-06-29 17:16:12 +0200

    creating dist folder

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCN-3V5L-M3HG8FFx2NTHfNxBarU8bYuHw",
+    authDomain: "phonebook-6d45e.firebaseapp.com",
+    databaseURL: "https://phonebook-6d45e.firebaseio.com",
+    projectId: "phonebook-6d45e",
+    storageBucket: "",
+    messagingSenderId: "917702501984",
+    appId: "1:917702501984:web:b02542ff41bf7246"
+  }
 };
